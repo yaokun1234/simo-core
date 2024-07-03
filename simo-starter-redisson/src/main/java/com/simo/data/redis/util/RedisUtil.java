b2abb40ed1ecc4877d2210f9d6b2a88cfafef6a0
Sonny Varquez <84620029+yaokun1234@users.noreply.github.com>
2024-07-03 17:53:36 +0800

    更新

---
+++ simo-starter-redisson/src/main/java/com/simo/data/redis/util/RedisUtil.java
@@ -0,0 +1,13 @@
+package com.simo.data.redis.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.locks.Lock;
+
+@Component
+@Slf4j
+public class RedisUtil {
+
+}
